84281be1349081cfb20afb09631f8636683747c6
Nikolai Lønberg <niklonberg@gmail.com>
2023-10-24 10:21:27 +0200

    cleaned up app.js code, adding comments, change variable names for clarification

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,28 @@
 const log = console.log;
 
+/* imports */
 import Book from "./Book.js";
 import Library from "./LibraryManager.js";
 
+/* class instantiation */
 const LibraryManager = new Library();
+const book1 = new Book("The Shining", "Stephen King");
+const book2 = new Book("Lord of the Rings", "Tolkien", 300, true);
 
-const userLibraryArray = [];
-const userLibrary = document.querySelector("#user-library");
+/* library instantiation */
+const userLibrary = [];
+
+/* dom element references */
+const userLibraryColl = document.querySelector("#user-library");
 const addBookBtn = document.querySelector("#add-book-btn");
 const bookModal = document.querySelector("#new-book-modal");
 const bookForm = document.querySelector("#book-form");
 const closeBookModalBtn = document.querySelector("#close-book-modal");
 
-const book1 = new Book("The Shining", "Stephen King");
-const book2 = new Book("Lord of the Rings", "Tolkien", 300, true);
+book1.addBookToLibrary(userLibrary);
+book2.addBookToLibrary(userLibrary);
 
-book1.addBookToLibrary(userLibraryArray);
-book2.addBookToLibrary(userLibraryArray);
-
-LibraryManager.insertBooks(userLibraryArray, userLibrary);
+LibraryManager.insertBooks(userLibrary, userLibraryColl);
 
 addBookBtn.addEventListener("click", () => {
   bookModal.showModal();
@@ -33,8 +37,8 @@ bookForm.addEventListener("submit", (event) => {
     bookForm.elements.author.value
   );
 
-  addBookToLibrary(userLibraryArray, newBook);
-  insertBooks(userLibraryArray, userLibrary);
+  addBookToLibrary(userLibrary, newBook);
+  insertBooks(userLibrary, userLibraryColl);
 
   bookModal.close();
 });
